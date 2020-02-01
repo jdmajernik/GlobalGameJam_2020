57791215c826352f1b4345a6bdb6b330730fdfd2
jdmajernik <jdmajernik@gmail.com>
2020-02-01 07:22:24 -0500

    Adding in prototype Fire object with a prototype object that spawns in the fire

---
+++ Assets/Scripts/Interactable/CombustableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombustableObject : InteractableObject
+{
+    private GameObject FireObject;
+    private GameObject SpawnedFire;
+
+    [SerializeField] private float FireSpawnZOffset = 2.0f;
+
+    void Awake()
+    {
+        FireObject = Resources.Load<GameObject>("Fire");
+    }
+    public override void OnBearInteract()
+    {
+        base.OnBearInteract();
+        SpawnFire();
+    }
+
+    protected virtual void SpawnFire()
+    {
+        if(SpawnedFire == null) SpawnedFire = Instantiate(FireObject, this.transform.position + new Vector3(0, 0,0 - FireSpawnZOffset), Quaternion.identity);
+    }
+}
