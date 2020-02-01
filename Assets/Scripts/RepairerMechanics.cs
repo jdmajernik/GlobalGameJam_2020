2dd1abee6cc892303546226114a938269cc0e65e
jdmajernik <jdmajernik@gmail.com>
2020-01-31 22:56:01 -0500

    added in empty GameManager and RepairerMechanics. The latter will control the mechanics behind the mouse player and I will be adding basic functionality to test the Interactible objets

---
+++ Assets/Scripts/RepairerMechanics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepairerMechanics : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("Fire1"))
+        {
+            Debug.Log("Pressing Fire key");
+        }
+    }
+}
