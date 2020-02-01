102c438f3447785e4d9f981aab5368da4b7f97ba
legender2 <43158716+legender2@users.noreply.github.com>
2020-02-01 01:15:27 -0500

    pointer + starterlogic

---
+++ Assets/Scripts/StartupLogic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupLogic : MonoBehaviour
+{
+
+
+
+    void Start()
+    {
+        Cursor.SetCursor(Resources.Load<Texture2D>("pointer"), new Vector2(22, 6), CursorMode.ForceSoftware);
+    }
+}
