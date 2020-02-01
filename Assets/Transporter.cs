f46d62c10285549788613673bd27c69fb74a7512
legender2 <43158716+legender2@users.noreply.github.com>
2020-01-31 23:57:38 -0500

    merge from transporter to main, probably broken

---
+++ Assets/Transporter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transporter : MonoBehaviour
+{
+    public enum TransporterInput { VerticalAxisUp, VerticalAxisDown }
+
+    [SerializeField] Transform endpoint;
+    [SerializeField] TransporterInput inputDirection;
+    [HideInInspector] public Transform Endpoint => endpoint;
+
+    Transform bear = null;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.root.GetComponent<BearController>())
+        {
+            bear = other.transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.GetComponent<BearController>())
+        {
+            bear = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (bear == null)
+        {
+            return;
+        }
+
+        switch (inputDirection)
+        {
+            case TransporterInput.VerticalAxisUp:
+                if (Input.GetAxisRaw("Vertical") > 0f)
+                {
+                    DoTransport(bear);
+                }
+                break;
+            case TransporterInput.VerticalAxisDown:
+                if (Input.GetAxisRaw("Vertical") < 0f)
+                {
+                    DoTransport(bear);
+                }
+                break;
+        }
+    }
+
+    void DoTransport(Transform target)
+    {
+        target.GetComponent<BearController>().Transport(endpoint.position);
+    }
+}
