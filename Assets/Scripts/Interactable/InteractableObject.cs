b7b5e5d8f393d845b3ced896abc72b609b7e3210
HankSnouwaert <54607969+HankSnouwaert@users.noreply.github.com>
2020-02-01 15:59:04 -0500

    Added repair time to breakable objects

--- Assets/Scripts/Interactable/InteractableObject.cs
+++ Assets/Scripts/Interactable/InteractableObject.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,14 +10,31 @@ public class InteractableObject : MonoBehaviour
 
     [SerializeField] private Material DefaultMaterial;
 
+    private bool AsyncLatch = false;
+
+    private bool RepairLatch = false;
+
+    private float TimerIncrement = 0.1f;
+
+    private int RepairIncrement = 10;
+
+    private int Durability = 100;
+
+    private int Damage = 0;
+
     private bool bIsDestroyed = false;
 
     public virtual void OnBearInteract()
     {
         if (!bIsDestroyed)
         {
-            this.bIsDestroyed = true;
-            this.gameObject.GetComponent<Renderer>().material = OnDestroyedMaterial;
+            this.Damage = this.Durability;
+            if (this.Damage >= this.Durability)  //The damage done can be modified later
+            {
+                this.Damage = this.Durability;
+                this.bIsDestroyed = true;
+                this.gameObject.GetComponent<Renderer>().material = OnDestroyedMaterial;
+            }
         }
     }
 
@@ -24,13 +42,54 @@ public class InteractableObject : MonoBehaviour
     {
         if (bIsDestroyed)
         {
-            this.bIsDestroyed = false;
-            this.gameObject.GetComponent<Renderer>().material = DefaultMaterial;
+            this.Damage = this.Damage - this.RepairIncrement;
+            if(this.Damage <= 0)
+            {
+                this.Damage = 0;
+                this.bIsDestroyed = false;
+                this.gameObject.GetComponent<Renderer>().material = DefaultMaterial;
+            }
         }
     }
 
-    void OnMouseDown()
+    void OnMouseOver()
     {
-        RepairerInteract();
+        if (Input.GetMouseButtonDown(0) && !AsyncLatch && bIsDestroyed == true)
+        {
+            StartCoroutine("OnTimerExecute");
+        }else if (Input.GetMouseButtonDown(1))
+        {
+            RepairLatch = false;
+        }
     }
-}
+    void OnMouseExit()
+    {
+        this.RepairLatch = false;
+    }
+
+
+    protected IEnumerator OnTimerExecute()
+    {
+        if (this.AsyncLatch)
+        {
+            yield break;
+        } //Check for prexisting asych thread
+    
+        while (Input.GetMouseButton(0))
+        {
+            this.AsyncLatch = true;
+            this.RepairLatch = true;
+
+            yield return new WaitForSeconds(TimerIncrement);
+
+            if (this.RepairLatch == true)
+            {
+                RepairerInteract();
+                this.RepairLatch = false;
+            }
+            this.AsyncLatch = false;
+        }
+    }
+
+
+}   
